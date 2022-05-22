efdd46a2a2a3d367d011cfefe77c80d438cd467e
Henrique Aurelio <henriqueaureliocsilva@gmail.com>
2022-05-22 00:49:40 -0300

    chore: structure

---
+++ server/src/useCases/CreateUser/CreateUserController.ts
@@ -0,0 +1,5 @@
+export class createUserController {
+  constructor(private createUserUseCase: CreateUserUseCase) {
+
+  }
+}
---
+++ server/src/useCases/CreateUser/CreateUserUseCase.ts
@@ -0,0 +1,3 @@
+export class CreateUserUseCase {
+  constructor(private usersRepository: IUsersRepository) { }
+}
